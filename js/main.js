ebcbdd8a0659ac2e6bdee876e7c6195e09fff7ce
Andrew-tih <andrew.tih@mailru>
2013-05-18 16:07:08 +0300

    New Changes
    Change Mobile , Table and Some DeskTop View

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 
+(function($){
+	/*
+	$(function(){
+		$("#menu").on("click.code",function(){
+			
+			var el=$(".nav-collapse.collapse");
+			var h;
+			el.on("shown",function(){
+				h=el.outerHeight();
+				$(".side-bar").css({top:("+="+h)});
+				console.log(h);
+			});
+			
+			el.on("hide",function(){
+				$(".side-bar").css({top:("-="+h)});
+				console.log(h);
+			});
+			
+			
+		});
+	});
+	*/
+})(jQuery);
