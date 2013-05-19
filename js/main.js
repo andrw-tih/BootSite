6ed5f3756355ce51ffe35fe80b9e0f30abee173d
Andrew-tih <andrew.tih@mailru>
2013-05-19 20:37:30 +0300

    Make Fluid
    change to fluid and add  some js code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 (function($){
-	/*
+	
 	$(function(){
 		$("#menu").on("click.code",function(){
 			
@@ -8,17 +8,76 @@
 			var h;
 			el.on("shown",function(){
 				h=el.outerHeight();
-				$(".side-bar").css({top:("+="+h)});
+				if(!$(".side-bar").hasClass("in"))
+				{
+				$(".side-bar").css({top:("+="+h)}).addClass("in");
+				
+				}
 				console.log(h);
 			});
 			
 			el.on("hide",function(){
-				$(".side-bar").css({top:("-="+h)});
+				if($(".side-bar").hasClass("in"))
+				{
+				$(".side-bar").css({top:("-="+h)}).removeClass("in");
+				}
 				console.log(h);
 			});
 			
 			
 		});
+		
+		
+		
+		$(Size);
+		$(window).resize(Size);
+		
+		function Size()
+		{
+			if($(window).width()>980){
+				var wa=400;
+				
+				var w=$(".page-container-f").outerWidth();
+				console.log(w);
+				var x=w/wa;
+				console.log(x);
+				if(x<=2)
+				{
+					$(".accordion").addClass("full-w");
+				}
+				else{
+					
+					
+					$(".accordion").removeClass("full-w");
+					
+						var marg=(w-parseInt(x)*wa)/(parseInt(x)*2);
+						marg-=parseInt(x)*2;
+						$(".accordion").css({"margin-left":marg,
+											"margin-right":marg})			
+					var ewrap=[];
+					if($(".accordion").parent().hasClass("clearfix"))
+						$(".accordion").unwrap();
+					$(".accordion").each(function(i,o){
+						
+						if(i!=0 && i%2==0)
+						{
+							$(ewrap).wrapAll("<div class='clearfix'>");
+							
+							ewrap=[];
+						}
+						ewrap.push(o);
+						
+						
+					});
+					$(ewrap).wrapAll("<div class='clearfix'>");
+					
+					
+				}
+			}
+		}
+		
+		
+		
 	});
-	*/
+	
 })(jQuery);
